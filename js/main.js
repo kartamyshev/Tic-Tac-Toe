b869d72b1e1fcd30a10be8d6b87bb6c06d1da11e
kartamyshev <kostya85>
2014-12-06 15:16:26 +0300

    horizontal checking implemented

--- js/main.js
+++ js/main.js
@@ -16,6 +16,15 @@ var pushDefaultValuesToMatrix = function () {
     }
 }();
 
+
+Array.prototype.allValuesSame = function() {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] === -1) return false;
+        if (this[i] !== this[0]) return false;
+    }
+    return true;
+};
+
 function showCurrentProgress() {
 	$('.current__text').text(function(){
 		return flag ? 'x' : 'o';
@@ -24,7 +33,40 @@ function showCurrentProgress() {
 showCurrentProgress();
 
 function checkWinner() {
-    console.log(matrix);
+    function winnerReload() {
+        alert('You are winner');
+        location.reload(true);
+    }
+    var horizontal = function() {
+        if (matrix[0].allValuesSame() || matrix[1].allValuesSame() || matrix[2].allValuesSame()) {
+            winnerReload();
+        }
+    };
+
+//    var vertical = function() {
+//        var arr = [[], [], []];
+//
+//        arr[0][0] = matrix[0][0] === matrix[1][0];
+//        arr[0][1] = matrix[1][0] === matrix[2][0];
+//        arr[0][2] = matrix[0][0] === matrix[2][0];
+//
+//        arr[1][0] = matrix[0][1] === matrix[1][1];
+//        arr[1][1] = matrix[1][1] === matrix[2][1];
+//        arr[1][2] = matrix[0][1] === matrix[2][1];
+//
+//        arr[2][0] = matrix[0][2] === matrix[1][2];
+//        arr[2][1] = matrix[1][2] === matrix[2][2];
+//        arr[2][2] = matrix[0][2] === matrix[2][2];
+//        if (arr[0].allValuesSame() || arr[1].allValuesSame() || arr[2].allValuesSame()) {
+//            winnerReload();
+//        }
+//    };
+
+    var diagonal = function() {};
+
+    horizontal();
+//    vertical();
+    diagonal();
 }
 
 cells.each(function(){
@@ -59,3 +101,5 @@ cells.each(function(){
     });
 
 });
+
+
